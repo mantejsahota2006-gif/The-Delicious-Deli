b906658d936319e73c9eea8f0f09570cad83133b
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-11-13 13:00:32 -0500

    Sandwich base prices

--- src/main/java/com/pluralsight/deli/PriceList.java
+++ src/main/java/com/pluralsight/deli/PriceList.java
@@ -1,4 +1,16 @@
 package com.pluralsight.deli;
 
-public class PriceList {
+public final class PriceList {
+    private PriceList() {
+    }
+
+    // Sandwich base prices by size
+    public static double baseSandwichPrice(int sizeInches) {
+        return switch (sizeInches) {
+            case 4 -> 5.50;
+            case 8 -> 7.00;
+            case 12 -> 8.50;
+            default -> throw new IllegalArgumentException("Invalid size: " + sizeInches);
+        };
+    }
 }
