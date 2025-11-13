9a16dbb6adc30f76741dab7cde76ce2407defebb
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-11-13 13:36:52 -0500

    Sauce Class

--- src/main/java/com/pluralsight/deli/Sauce.java
+++ src/main/java/com/pluralsight/deli/Sauce.java
@@ -1,4 +1,13 @@
 package com.pluralsight.deli;
 
 public class Sauce extends Topping {
+    public Sauce(String name) {
+        super(name, false);
+    }
+
+    @Override
+    public double getPrice(int sizeInches) {
+        // Sauces are included
+        return 0.0;
+    }
 }
