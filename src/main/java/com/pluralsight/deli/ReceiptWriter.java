7a900ac79a2f076f31984ccabc2f1b39a0022486
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-11-13 18:22:34 -0500

    Receipt writer class

--- src/main/java/com/pluralsight/deli/ReceiptWriter.java
+++ src/main/java/com/pluralsight/deli/ReceiptWriter.java
@@ -1,4 +1,21 @@
 package com.pluralsight.deli;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.format.DateTimeFormatter;
+
 public class ReceiptWriter {
+    private static final DateTimeFormatter FILE_FMT = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+
+    public static Path saveOrderReceipt(Order order) throws IOException {
+        Path receiptsDir = Path.of("receipts");
+        if (!Files.exists(receiptsDir)) Files.createDirectories(receiptsDir);
+
+        String filename = order.getOrderDate().format(FILE_FMT) + ".txt";
+        Path file = receiptsDir.resolve(filename);
+
+        Files.writeString(file, order.toReceiptString());
+        return file;
+    }
 }
