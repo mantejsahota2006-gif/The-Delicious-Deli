ca6c86acafbf0d810a622287844026cc0ee3ed61
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-11-13 13:09:40 -0500

    Premium topping add-ons

--- src/main/java/com/pluralsight/deli/PriceList.java
+++ src/main/java/com/pluralsight/deli/PriceList.java
@@ -4,13 +4,23 @@ public final class PriceList {
     private PriceList() {
     }
 
-    // Sandwich base prices by size
+    // Sandwich base prices
     public static double baseSandwichPrice(int sizeInches) {
         return switch (sizeInches) {
             case 4 -> 5.50;
             case 8 -> 7.00;
             case 12 -> 8.50;
             default -> throw new IllegalArgumentException("Invalid size: " + sizeInches);
+        };
+    }
+
+    // Premium topping add-ons meat & cheese
+    public static double meatAddOn(int sizeInches) {
+        return switch (sizeInches) {
+            case 4 -> 1.00;
+            case 8 -> 2.00;
+            case 12 -> 3.00;
+            default -> 0.0;
         };
     }
 }
