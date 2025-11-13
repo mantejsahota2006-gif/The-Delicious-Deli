2e90d3966fd45e7f969de6685546f7e7d29a8699
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-11-13 13:38:45 -0500

    Sandwich Variables

--- src/main/java/com/pluralsight/deli/Sandwich.java
+++ src/main/java/com/pluralsight/deli/Sandwich.java
@@ -1,4 +1,18 @@
 package com.pluralsight.deli;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class Sandwich {
+    private int size;                 // 4, 8, or 12
+    private String breadType;         // white, wheat, rye, wrap
+    private boolean toasted;
+    private final List<Topping> toppings = new ArrayList<>();
+
+    public Sandwich(int size, String breadType, boolean toasted) {
+        this.size = size;
+        this.breadType = breadType;
+        this.toasted = toasted;
+    }
 }
